06998c6c43bbe44a9914d59fa79b962aacd22085
xxihye <ekfhdwld@gmail.com>
2025-07-14 15:43:42 +0900

    [auth-service] feat: add SecurityConfig

---
+++ auth-service/src/main/java/com/urlshortener/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.urlshortener.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    // 비밀번호 암호화 Bean
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
