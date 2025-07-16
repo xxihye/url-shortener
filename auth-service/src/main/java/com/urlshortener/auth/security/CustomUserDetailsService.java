3c1c8b92876154bc3f4b8bb5398f7fc06356af5c
xxihye <ekfhdwld@gmail.com>
2025-07-16 11:32:04 +0900

    [auth-service] refactor: update CustomUserDetailsService

---
+++ auth-service/src/main/java/com/urlshortener/auth/security/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.urlshortener.auth.security;
+
+import com.urlshortener.admin.repository.AdminUserRepository;
+import com.urlshortener.auth.enums.Role;
+import com.urlshortener.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final AdminUserRepository adminUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+
+        return userRepository.findByUserId(userName)
+                             .map(user -> new UserPrincipal(user.getUserNo(),
+                                                             user.getUserId(),
+                                                             user.getPassword(),
+                                                             Role.ROLE_USER))
+                             .orElseGet(() -> adminUserRepository.findByAdminId(userName)
+                                                                 .map(admin -> new UserPrincipal(admin.getAdminNo(),
+                                                                                                 admin.getAdminId(),
+                                                                                                 admin.getPassword(),
+                                                                                                 Role.ROLE_ADMIN))
+                                                                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."))
+                             );
+    }
+}
